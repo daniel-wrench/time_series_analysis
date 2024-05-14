dac6b4b1ce55bcc8f3bb6a698af291366a123165
Daniel Wrench <daniel@harmonic.co.nz>
2024-05-14 13:26:27 +1200

    fixing flipbook sf, adding lag highlighting, powerlaws

--- plotz_tq.py
+++ plotz_tq.py
@@ -20,8 +20,9 @@ import TurbAn.Analysis.Simulations.AnalysisFunctions as af
 # have commented out import in TurbAn\Analysis\Simulations\AnalysisFunctions.py and TurbAn\Analysis\TimeSeries\Time_Series_Analysis.py
 
 
-def plotz(tfiles, rfiles, name, plotpoints, timestamp):
-    pdf = PdfPages("plots/stats_" + name + "_" + timestamp + ".pdf")
+def plotz(tfiles, rfiles, name, var_name, unit, spacecraft, plotpoints, timestamp):
+    pdf = PdfPages("plots/stats_" + spacecraft + name + "_" + timestamp + ".pdf")
+    lags_to_examine = [5, 50, 500]
     for flt, flr in zip(tfiles, rfiles):
         if not os.path.exists(flt) or not os.path.exists(flr):
             print("File path does not exist:", flt, flr)
@@ -38,6 +39,7 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
             names = [name]
         #     try:
         plt.clf()
+        plt.figure(figsize=(14, 6))
         ax0 = plt.subplot2grid((2, 4), (0, 0), colspan=3)
         ax1 = plt.subplot2grid((2, 4), (0, 3), colspan=1)
         ax2 = plt.subplot2grid((2, 4), (1, 0))
@@ -50,6 +52,7 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
             ax0.plot(dr.index[::jumps], dr[var][::jumps], ".-", label=var)
             # Plot histogram of the raw time series
             ax1.hist(dr[var], bins=50, alpha=0.5, label=var)
+            ax1.set_xlabel(unit)
             # Overlay an "equivalent Gaussian" (same mean and variance)
             x_vals = np.linspace(dr[var].min(), dr[var].max(), 100)
             y_vals = (
@@ -64,17 +67,22 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
                 linestyle=":",
                 # label=r"$N(\mu, \sigma^2)$",
             )
+
+            # Remove y-axis tickmarks from the histogram
+            ax1.yaxis.set_ticks_position("none")
+            ax1.yaxis.set_ticklabels([])
+
             # Annotate the values of the first four moments of the distributions
             if len(names) == 1:
                 ax1.annotate(
                     rf"$\mu$={dr[var].mean():.2f}",
-                    (0.05, 0.8),
+                    (0.05, 0.85),
                     xycoords="axes fraction",
                     fontsize=9,
                 )
                 ax1.annotate(
                     rf"$\sigma^2$={dr[var].var():.2f}",
-                    (0.05 + 0.2 * i, 0.7),
+                    (0.05 + 0.2 * i, 0.75),
                     xycoords="axes fraction",
                     fontsize=9,
                     bbox=dict(
@@ -87,7 +95,7 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
                 )
                 ax1.annotate(
                     f"Skew={dr[var].skew():.2f}",
-                    (0.05, 0.6),
+                    (0.05, 0.65),
                     xycoords="axes fraction",
                     fontsize=9,
                     bbox=dict(
@@ -100,7 +108,7 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
                 )
                 ax1.annotate(
                     f"Kurt={(dr[var].kurtosis()+3):.2f}",  # Add 3 to get actual kurtosis
-                    (0.05, 0.5),
+                    (0.05, 0.55),
                     xycoords="axes fraction",
                     fontsize=9,
                     bbox=dict(
@@ -113,12 +121,13 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
                 )
 
         # ax0.set_xlabel("Time")
-        ax0.set_ylabel(name)
+        ax0.set_ylabel(unit)
         ax0.xaxis.set_major_formatter(
-            mdates.ConciseDateFormatter(ax.xaxis.get_major_locator())
+            mdates.ConciseDateFormatter(ax0.xaxis.get_major_locator())
         )
         ax0.set_title(
-            f"Statistics for time series of ${name}$",  #  beginning {str(dr.index[0])}
+            "Time series statistics of $\\mathbf{%s}$" % var_name,
+            #  beginning {str(dr.index[0])}
             fontsize=14,
         )
         ax1.set_title("Distribution and moments")
@@ -139,6 +148,17 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
         ax2.set_ylim(
             np.percentile(d[name]["power_periodogram"], 0.01),
             10 * np.max(d[name]["power_periodogram"]),
+        )
+        af.pltpwrl(
+            1e-3,
+            d[name]["p_smooth"][5] * 80.0,
+            xi=1e-2,
+            xf=1,
+            alpha=-5.0 / 3,
+            label="-5/3",
+            ax=ax2,
+            color="purple",
+            ls="--",
         )
         ax2.set_xlabel("Frequency (Hz)")
 
@@ -195,19 +215,35 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
         # ax3.plot(d[name]['times'],d[name]['cr']/3.,label='ACF')
         # tcor=d[name]['times'][np.argmin(np.abs(d[name]['cr']/3.-1./np.e))]
         # else:
-        ax3.plot(d[name]["times"], d[name]["cr"], label="ACF")
+        ax3.plot(d[name]["times"][: len(d[name]["cr"])], d[name]["cr"], label="ACF")
         # tcor=d[name]['times'][np.argmin(np.abs(d[name]['cr']-1./np.e))]
         tcor = d[name]["tce"]
         # Correlation scale is available in the stats file, don't need to calculate here
         ax3.axvline(tcor, linestyle="--", c="black")
-        ax3.text(tcor * 1.1, 0.8, r"$t_{{corr}}$ = {0:.02f} s".format(tcor))
+        ax3.text(tcor * 1.2, 0.8, r"$t_{{corr}}$")  # = {0:.2f} s".format(tcor))
         ax3.set_xlabel("$\Delta$t (s)")
         # ax3.legend()
 
         # Plot the structure function sfn
-        ax4.loglog(d[name]["sfn"].iloc[:, 2], label="S$^{(2)}$")
-        yyy = d[name]["sfn"].iloc[:, 2].values[58]
-        af.pltpwrl(50, yyy * 2.0, xi=1, xf=500, alpha=2.0 / 3, label="2/3", ax=ax4)
+        ax4.plot(
+            d[name]["times"][: len(d[name]["sfn"])],
+            d[name]["sfn"].loc[:, "2"],
+            label="S$^{(2)}$",
+        )
+        ax4.set_xscale("log")
+        ax4.set_yscale("log")
+        yyy = d[name]["sfn"].loc[:, "2"].values[58]
+        af.pltpwrl(
+            50,
+            yyy * 2.0,
+            xi=1,
+            xf=500,
+            alpha=2.0 / 3,
+            label="2/3",
+            ax=ax4,
+            color="purple",
+            ls="--",
+        )
         # yy2=d[name]['sfn'].iloc[:,2].values[1]*1.1
         # ax4.text(3,yy2,r'S$^{{(2)}}(dt_{{max}})$={0:.03e}'.format(d[name]['sfn'][0,-1]))
         ax4.set_xlabel("$\Delta$t (s)")
@@ -220,7 +256,12 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
         #    ax5.semilogx(d[names[1]]['tau'],d[names[1]]['sdk'],label='$\kappa_{'+names[1]+'}$')
         #    ax5.semilogx(d[names[2]]['tau'],d[names[2]]['sdk'],label='$\kappa_{'+names[2]+'}$')
         # else:
-        ax5.semilogx(d[name]["sdk"], label="$\kappa_{" + name + "}$")
+        ax5.plot(
+            d[name]["times"][: len(d[name]["sdk"])],
+            d[name]["sdk"],
+            label="$\kappa_{" + name + "}$",
+        )
+        ax5.set_xscale("log")
         # Add a horizontal linae at k =3
         ax5.axhline(3, color="black", linestyle="--")
         ax5.set_xlabel("$\Delta$t (s)")
@@ -240,7 +281,16 @@ def plotz(tfiles, rfiles, name, plotpoints, timestamp):
         # ax5.legend()
         # ax5.set_xlabel(r'$\Delta$'+names[0]+r'$/\sigma_{\Delta '+names[0]+'}$')
         # ax5.set_ylabel('PDF')
-
+        for lag in lags_to_examine:
+            for ax in [ax3, ax4, ax5]:
+                # Multiplying by re-sampled freq to get correct placement
+                ax.axvline(
+                    lag * d[name]["times"][1],
+                    color="red",
+                    linestyle="--",
+                    alpha=0.2,
+                    lw=0.5,
+                )
         # plt.tight_layout()
         pdf.savefig(bbox_inches="tight")
         plt.close()
@@ -264,6 +314,9 @@ plotz(
     tfiles=glob.glob("data/processed/wind/B_turb_" + timestamp + ".pkl"),
     rfiles=glob.glob("data/processed/wind/B_raw_" + timestamp + ".pkl"),
     name="B",
+    var_name="solar\ wind\ magnetic\ field\ from\ Wind",
+    unit="B (nT)",
+    spacecraft="wind",
     plotpoints=400,
     timestamp=timestamp,
 )
@@ -272,6 +325,20 @@ plotz(
     tfiles=glob.glob("data/processed/wind/Bx_turb_" + timestamp + ".pkl"),
     rfiles=glob.glob("data/processed/wind/Bx_raw_" + timestamp + ".pkl"),
     name="Bx",
+    var_name="solar\ wind\ magnetic\ field\ from\ Wind",
+    unit="Bx (nT)",
+    spacecraft="wind",
+    plotpoints=400,
+    timestamp=timestamp,
+)
+
+plotz(
+    tfiles=glob.glob("data/processed/psp/B_R_turb_" + timestamp + ".pkl"),
+    rfiles=glob.glob("data/processed/psp/B_R_raw_" + timestamp + ".pkl"),
+    name="B_R",
+    var_name="solar\ wind\ magnetic\ field\ from\ PSP",
+    unit="B_R (nT)",
+    spacecraft="psp",
     plotpoints=400,
     timestamp=timestamp,
 )
@@ -280,22 +347,25 @@ plotz(
     tfiles=glob.glob("data/processed/wind/fbm_turb" + ".pkl"),
     rfiles=glob.glob("data/processed/wind/fbm_raw" + ".pkl"),
     name="fbm",
+    var_name="multi-fractal\ fractional\ Brownian\ motion",
+    unit="",
+    spacecraft="",
     plotpoints=400,
     timestamp=timestamp,
 )
 
-plotz(
-    tfiles=glob.glob("data/processed/wind/V_turb_" + timestamp + ".pkl"),
-    rfiles=glob.glob("data/processed/wind/V_raw_" + timestamp + ".pkl"),
-    name="V",
-    plotpoints=400,
-    timestamp=timestamp,
-)
+# plotz(
+#     tfiles=glob.glob("data/processed/wind/V_turb_" + timestamp + ".pkl"),
+#     rfiles=glob.glob("data/processed/wind/V_raw_" + timestamp + ".pkl"),
+#     name="V",
+#     plotpoints=400,
+#     timestamp=timestamp,
+# )
 
-plotz(
-    tfiles=glob.glob("data/processed/wind/np_turb_" + timestamp + ".pkl"),
-    rfiles=glob.glob("data/processed/wind/np_raw_" + timestamp + ".pkl"),
-    name="np",
-    plotpoints=400,
-    timestamp=timestamp,
-)
+# plotz(
+#     tfiles=glob.glob("data/processed/wind/np_turb_" + timestamp + ".pkl"),
+#     rfiles=glob.glob("data/processed/wind/np_raw_" + timestamp + ".pkl"),
+#     name="np",
+#     plotpoints=400,
+#     timestamp=timestamp,
+# )
